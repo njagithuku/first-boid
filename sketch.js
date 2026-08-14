85ac4273954e266cffd1cb707a4751ee1627021c
njagithuku <njagithuku@gmail.com>
2026-08-14 20:46:19 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,49 +1,57 @@
-let flock = []; // empty list
+let flock = [];
 
 function setup() {
   createCanvas(400, 400);
   
-  // "for loop" that repeats 100 times
   for (let i = 0; i < 100; i++) {
-    flock.push(new Boid()); // Stamp out a new boid and add it to the list
+    flock.push(new Boid());
   }
 }
 
 function draw() {
   background(220);
   
-  // Tell every boid in the flock list to act
   for (let boid of flock) {
     boid.move();
     boid.show();
   }
 }
 
-// blueprint class
+//UPDATED BLUEPRINT
 class Boid {
-  // 'constructor' 
   constructor() {
-    this.x = random(width);      // Start at a random X on the screen
-    this.y = random(height);     // Start at a random Y on the screen
-    this.speedX = random(-3, 3); // Random speed left or right
-    this.speedY = random(-3, 3); // Random speed up or down
-  }
-  
-  //instructions for how a boid moves
-  move() {
-    this.x = this.x + this.speedX;
-    this.y = this.y + this.speedY;
+    // createVector bundles X and Y together
+    this.position = createVector(random(width), random(height));
     
-    // Pac-Man wrapping (using 'width' and 'height' instead of hard numbers)
-    if (this.x > width) this.x = 0;
-    if (this.x < 0) this.x = width;
-    if (this.y > height) this.y = 0;
-    if (this.y < 0) this.y = height;
+    // Velocity is our speedX and speedY bundled together
+    this.velocity = createVector(random(-3, 3), random(-3, 3));
   }
   
-  // instructions 
+  move() {
+    // Instead of doing x + speedX and y + speedY, Vectors do it in one line:
+    this.position.add(this.velocity);
+    
+    // Pac-Man wrapping
+    if (this.position.x > width) this.position.x = 0;
+    if (this.position.x < 0) this.position.x = width;
+    if (this.position.y > height) this.position.y = 0;
+    if (this.position.y < 0) this.position.y = height;
+  }
+  
   show() {
-    fill(50, 150, 255); // Colors it blue!
-    circle(this.x, this.y, 10);
+    // 1. Ask the velocity vector what angle it is pointing
+    let angle = this.velocity.heading();
+    
+    // 2. Rotation Trick
+    push(); // Save the normal, un-rotated canvas
+    
+    translate(this.position.x, this.position.y); // Move our paintbrush to the boid's location
+    rotate(angle); // Spin the canvas to match the boid's angle
+    
+    // Draw a triangle pointing to the right (which matches our angle)
+    fill(50, 150, 255);
+    triangle(10, 0, -5, 5, -5, -5); 
+    
+    pop(); // Restore the canvas back to normal
   }
 }
